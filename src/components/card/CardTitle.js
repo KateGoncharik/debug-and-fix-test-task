f57534801e365c8f37b9ca8b49d4a0dda2ba03a5
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-11-17 10:51:04 +0300

    fix: correct card title view

--- src/components/card/CardTitle.js
+++ src/components/card/CardTitle.js
@@ -21,8 +21,6 @@ const StyledCardTitle = styled.h2`
   margin-right: 8px;
   transition: color 0.3s;
   overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
   max-width: ${window.screen.width < 450 ? '130px' : '100%'};
   font-size: ${window.screen.width < 450 ? '18px' : '24px'};
 `;
