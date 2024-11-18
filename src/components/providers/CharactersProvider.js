64a53b5f5811addb5e723094f6207bc867dae548
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-11-18 10:50:03 +0300

    fix: handle backand forward browser buttons click

--- src/components/providers/CharactersProvider.js
+++ src/components/providers/CharactersProvider.js
@@ -11,21 +11,22 @@ import { firstPageNumber } from '../Pagination';
 
 const API_URL = 'https://rickandmortyapi.com/api/character/';
 
+const getInitSearchParamValue = (searchParamName, initSearchParams) => {
+  if (searchParamName === 'page') {
+    const pageNumberFromURL = +initSearchParams.get(searchParamName);
+
+    if (isNaN(pageNumberFromURL) || pageNumberFromURL < firstPageNumber) {
+      return firstPageNumber;
+    }
+    return pageNumberFromURL;
+  }
+  return initSearchParams.get(searchParamName) ?? '';
+};
+
 export function CharactersProvider({ children }) {
   const initSearchParams = new URLSearchParams(document.location.search);
 
-  const getInitSearchParamValue = (searchParamName) => {
-    if (searchParamName === 'page') {
-      const pageNumberFromURL = +initSearchParams.get(searchParamName);
-
-      if (isNaN(pageNumberFromURL) || pageNumberFromURL < firstPageNumber) {
-        return firstPageNumber;
-      }
-      return pageNumberFromURL;
-    }
-    return initSearchParams.get(searchParamName) ?? '';
-  };
-  const initPage = getInitSearchParamValue('page');
+  const initPage = getInitSearchParamValue('page', initSearchParams);
   const [activePage, setActivePage] = useState(initPage);
   const [characters, setCharacters] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
@@ -34,31 +35,43 @@ export function CharactersProvider({ children }) {
   const [apiURL, setApiURL] = useState(API_URL);
 
   const [filters, setFilters] = useState({
-    name: getInitSearchParamValue('name'),
-    status: getInitSearchParamValue('status'),
-    species: getInitSearchParamValue('species'),
-    type: getInitSearchParamValue('type'),
-    gender: getInitSearchParamValue('gender')
+    name: getInitSearchParamValue('name', initSearchParams),
+    status: getInitSearchParamValue('status', initSearchParams),
+    species: getInitSearchParamValue('species', initSearchParams),
+    type: getInitSearchParamValue('type', initSearchParams),
+    gender: getInitSearchParamValue('gender', initSearchParams)
   });
 
   const resetActivePage = () => {
     setActivePage(firstPageNumber);
   };
 
-  const updateUrl = useCallback(
-    (newFilters, page = activePage) => {
-      const searchParams = new URLSearchParams();
-      Object.entries(newFilters).forEach(([key, value]) => {
-        if (value) {
-          searchParams.set(key, value);
-        }
-      });
+  useEffect(() => {
+    const syncStateWithURL = () => {
+      const searchParams = new URLSearchParams(window.location.search);
 
-      searchParams.set('page', page.toString());
-      window.history.pushState({}, '', `?${searchParams.toString()}`);
-    },
-    [activePage]
-  );
+      const newFilters = {
+        name: searchParams.get('name') || '',
+        status: searchParams.get('status') || '',
+        species: searchParams.get('species') || '',
+        type: searchParams.get('type') || '',
+        gender: searchParams.get('gender') || ''
+      };
+
+      const newPage = +searchParams.get('page') || firstPageNumber;
+
+      setFilters((prev) =>
+        JSON.stringify(prev) !== JSON.stringify(newFilters) ? newFilters : prev
+      );
+      setActivePage((prev) => (prev !== newPage ? newPage : prev));
+    };
+
+    window.addEventListener('popstate', syncStateWithURL);
+
+    syncStateWithURL();
+
+    return () => window.removeEventListener('popstate', syncStateWithURL);
+  }, []);
 
   const fetchCharacters = useCallback(async (filters, page) => {
     setIsFetching(true);
@@ -83,9 +96,21 @@ export function CharactersProvider({ children }) {
   }, []);
 
   useEffect(() => {
+    const searchParams = new URLSearchParams();
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value) {
+        searchParams.set(key, value);
+      }
+    });
+    searchParams.set('page', activePage.toString());
+
+    const newUrl = `?${searchParams.toString()}`;
+    if (window.location.search !== newUrl) {
+      window.history.pushState({}, '', newUrl);
+    }
+
     fetchCharacters(filters, activePage);
-    updateUrl(filters, activePage);
-  }, [filters, activePage, updateUrl, fetchCharacters]);
+  }, [filters, activePage, fetchCharacters]);
 
   const dataValue = useMemo(
     () => ({
